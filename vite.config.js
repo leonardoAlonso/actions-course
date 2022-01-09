6c6c705180641902daddc1473b02441f42b29628
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-01-09 16:11:13 -0600

    fix base url setting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://leonardoalonso.github.io/',
+    base: 'https://leonardoalonso.github.io/actions-course/',
     plugins: [react()]
 })
